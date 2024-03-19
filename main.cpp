68c956c6b78d030e595ddba8fedb35547d4c92eb
Kazuma <21kazuma21@gmail.com>
2024-03-19 15:35:36 +0900

    最初のコミット

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+// 12/26 セクション3-2文字と数値の出力まで
+// 12/27 セクション3-6小数を扱う方法まで　次回セクション4変数
+// 1/3 セクション4-7 複合代入演算子
+// 1/3 セクション4終了　次回セクション5　条件分岐
+// 1/7 セクション5、6終了　次回セクション7 関数
+// 1/11 セクション7関数　終了　次回セクション8　配列
+// 2/15 セクション8配列　終了　次回セクション9　ポインタ
+
+int main(void){
+  int apple = 10;
+  int *p;
+  p = &apple;
+  printf("%d\n",*p);
+  return 0;
+}
